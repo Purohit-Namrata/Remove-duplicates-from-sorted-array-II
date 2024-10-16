2baeadcc6f4fed082743a268c1c2215971ba45ea
Purohit-Namrata <namratapurohit78@gmail.com>
2024-10-16 20:06:39 +0530

    Add files via upload

---
+++ Remove duplicates from sorted array II.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j=1
+        count=1
+        n=len(nums)
+        for i in range(1,n):
+            if nums[i]==nums[i-1]:
+                count+=1
+            else:
+                count=1
+            if count<=2:
+                nums[j]=nums[i]
+                j+=1
+        return j
+    
+s=Solution()
+print(s.removeDuplicates([1,1,1,2,2,2,2,3]))
